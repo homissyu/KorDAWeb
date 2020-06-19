1b2911ca62f49e0457ed4179df9d733daede4af8
Karl Choi <karlchoi@Karlui-MacBook-Pro.local>
2020-06-19 13:30:18 +0900

    add New Item

--- routes/newsDatum.js
+++ routes/newsDatum.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 var client_id = '3aVbezRKkHUsAHv7IQ9N';
 var client_secret = 'KdT0HQTnWc';
-var api_url = "https://openapi.naver.com/v1/search/news?display=100&query=%EA%B2%BD%EC%A0%9C+%EA%B8%88+%EC%A6%9D%EA%B6%8C+%EC%9E%90%EC%82%B0+%ED%88%AC%EC%9E%90";
+var api_url = "https://openapi.naver.com/v1/search/news?display=100&query=%E9%87%91%EA%B1%B0%EB%9E%98%EC%86%8C+%7C+%EC%84%BC%EA%B3%A8%EB%93%9C+%7C+%EA%B8%88%EA%B1%B0%EB%9E%98+%7C+%EA%B8%88%ED%88%AC%EC%9E%90+%7C+%EA%B8%88%EC%8B%9C%EC%84%B8";
 //   var api_url = 'https://openapi.naver.com/v1/search/blog.xml?query=' + encodeURI(req.query.query); // xml 결과
 var options = {
     url: api_url,
